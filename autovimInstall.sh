53fcbdbecf4dbb3fa6e62295689623860c679b6d
Md. Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2024-12-31 12:02:42 +0600

    Update autovimInstall.sh

--- autovimInstall.sh
+++ autovimInstall.sh
@@ -1,81 +1,85 @@
 #!/bin/bash
 
-# Update and install dependencies
-echo "Updating package list and installing dependencies..."
-sudo apt update && sudo apt install -y \
-  neovim \
-  git \
-  curl \
-  build-essential \
-  cmake \
-  python3-pip \
-  xclip  # For clipboard support
+set -e  # Exit immediately if a command exits with a non-zero status.
 
-# Install vim-plug (plugin manager)
-echo "Installing vim-plug..."
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "Starting Neovim setup for C++ competitive programming on Ubuntu..."
 
-# Create Neovim configuration directory
+# 1. Update system and install prerequisites
+echo "Updating system and installing prerequisites..."
+#sudo apt update && sudo apt upgrade -y
+sudo apt install -y neovim gcc g++ clang python3 python3-pip nodejs npm git curl unzip ripgrep build-essential
+
+# 2. Install pynvim for Python support in Neovim
+echo "Installing pynvim..."
+#pip3 install --user pynvim
+
+# 3. Install Lazy.nvim (Plugin Manager)
+echo "Installing Lazy.nvim..."
+git clone https://github.com/folke/lazy.nvim.git ~/.local/share/nvim/lazy/lazy.nvim
+
+# 4. Create Neovim configuration directory and init.lua
+echo "Setting up Neovim configuration..."
 mkdir -p ~/.config/nvim
 
-# Write Neovim configuration for competitive programming
-cat <<EOF > ~/.config/nvim/init.vim
-" Basic Neovim settings
-set number          " Show line numbers
-set relativenumber  " Relative line numbers
-set tabstop=4       " Tab width
-set shiftwidth=4    " Indent width
-set expandtab       " Convert tabs to spaces
-set autoindent      " Auto indent new lines
-set smartindent     " Smarter indentation
-set clipboard=unnamedplus " Use system clipboard
-syntax on           " Enable syntax highlighting
-filetype plugin indent on " Enable file type detection and specific plugins
+cat > ~/.config/nvim/init.lua << 'EOF'
+-- Lazy.nvim setup
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-" Plugin management with vim-plug
-call plug#begin('~/.local/share/nvim/plugged')
+-- Plugin setup
+require("lazy").setup({
+  {"neovim/nvim-lspconfig"}, -- LSP configuration
+  {"williamboman/mason.nvim"}, -- LSP/DAP installer
+  {"williamboman/mason-lspconfig.nvim"}, -- Mason + LSPConfig bridge
+  {"hrsh7th/nvim-cmp"}, -- Completion framework
+  {"hrsh7th/cmp-nvim-lsp"}, -- LSP completion
+  {"hrsh7th/cmp-buffer"}, -- Buffer completion
+  {"hrsh7th/cmp-path"}, -- File path completion
+  {"saadparwaiz1/cmp_luasnip"}, -- Snippet completion
+  {"L3MON4D3/LuaSnip"}, -- Snippet engine
+  {"rafamadriz/friendly-snippets"}, -- Predefined snippets
+  {"mfussenegger/nvim-dap"}, -- Debug Adapter Protocol
+  {"rcarriga/nvim-dap-ui"}, -- DAP UI
+  {"nvim-telescope/telescope.nvim", dependencies = {"nvim-lua/plenary.nvim"}}, -- Fuzzy Finder
+  {"nvim-tree/nvim-tree.lua"}, -- File explorer
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}, -- Treesitter for syntax
+  {"vim-test/vim-test"}, -- Test runner
+  {"lewis6991/gitsigns.nvim"}, -- Git integration
+  {"tpope/vim-commentary"}, -- Commenting
+  {"akinsho/toggleterm.nvim"}, -- Terminal integration
+})
 
-" Plugins
-Plug 'neoclide/coc.nvim', {'branch': 'release'}  " Completion and language server
-Plug 'preservim/nerdtree'                       " File navigation
-Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'} " Syntax highlighting
-Plug 'vim-airline/vim-airline'                  " Status bar
+-- Mason setup
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = {"clangd"},
+})
 
-call plug#end()
+-- LSP configuration
+local lspconfig = require("lspconfig")
+lspconfig.clangd.setup({})
 
-" COC (Conqueror of Completion) setup
-let g:coc_global_extensions = ['coc-clangd']
+-- Key mappings
+vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>tt", "<cmd>TestNearest<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>dd", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>dc", "<cmd>lua require'dap'.continue()<cr>", { noremap = true, silent = true })
 
-" Key mappings
-nnoremap <C-n> :NERDTreeToggle<CR>  " Ctrl+n to toggle file tree
 EOF
 
-# Install plugins via Neovim
-echo "Installing plugins..."
-nvim +PlugInstall +qall
+# 5. Install LSP tools using Mason
+echo "Installing LSP tools..."
+nvim --headless -c "Lazy sync" -c "qa"
 
-# Install and configure COC language server
-echo "Setting up C++ language server..."
-sudo apt install -y clangd
-echo 'export PATH="/usr/bin/clangd:$PATH"' >> ~/.bashrc
-source ~/.bashrc
-
-# Completion setup for COC
-cat <<EOF > ~/.config/nvim/coc-settings.json
-{
-  "languageserver": {
-    "clangd": {
-      "command": "clangd",
-      "filetypes": ["c", "cpp", "objc", "objcpp"],
-      "rootPatterns": ["compile_flags.txt", "compile_commands.json", ".git/"],
-      "initializationOptions": {
-        "fallbackFlags": ["-std=c++17"]
-      }
-    }
-  }
-}
-EOF
-
-# Finish setup
-echo "Setup complete! Launch Neovim with 'nvim' and enjoy competitive programming in C++."
+echo "Setup complete! Launch Neovim with 'nvim' and start coding!"
